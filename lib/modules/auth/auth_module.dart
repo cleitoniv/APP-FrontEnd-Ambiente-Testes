0a11c2441a2aced2280c396075b24905512391eb
Elioenai Ferrari <elioenaiferrari@gmail.com>
2020-05-22 11:48:04 -0300

    update

--- lib/modules/auth/auth_module.dart
+++ lib/modules/auth/auth_module.dart
@@ -1,4 +1,3 @@
-import 'package:central_oftalmica_app_cliente/config/client_http.dart';
 import 'package:central_oftalmica_app_cliente/modules/auth/create_account_screen.dart';
 import 'package:central_oftalmica_app_cliente/modules/auth/login_screen.dart';
 import 'package:central_oftalmica_app_cliente/modules/auth/password_reset_screen.dart';
