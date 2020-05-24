40bd865fd0c04e169dbc0c298969ec219063f651
Elioenai Ferrari <elioenaiferrari@gmail.com>
2020-05-24 15:45:30 -0300

    bloc interface

---
+++ lib/bloc/bloc.dart
@@ -0,0 +1,16 @@
+abstract class BlocBase<Model> {
+  Sink get indexIn => throw UnimplementedError();
+  Stream<List<Model>> get indexOut => throw UnimplementedError();
+
+  Sink get showIn => throw UnimplementedError();
+  Stream<Model> get showOut => throw UnimplementedError();
+
+  Sink get storeIn => throw UnimplementedError();
+  Stream<String> get storeOut => throw UnimplementedError();
+
+  Sink get updateIn => throw UnimplementedError();
+  Stream<String> get updateOut => throw UnimplementedError();
+
+  Sink get deleteIn => throw UnimplementedError();
+  Stream<String> get deleteOut => throw UnimplementedError();
+}
